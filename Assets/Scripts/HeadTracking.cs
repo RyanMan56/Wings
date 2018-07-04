91165956e1a4bf18da463809e96b85474ded6d12
Ryan Jones <RyanMan56@gmail.com>
2018-07-04 21:27:22 +0100

    Player collision box done, adjusts to height while maintaining radius

--- Assets/Scripts/HeadTracking.cs
+++ Assets/Scripts/HeadTracking.cs
@@ -7,10 +7,11 @@ public class HeadTracking : MonoBehaviour
 {
     private readonly XRNode nodeType = XRNode.Head;
     public CapsuleCollider playerCollider;
+    private SphereCollider headCollider;
 
 	// Use this for initialization
 	void Start () {
-		
+        headCollider = GetComponent<SphereCollider>();
 	}
 	
 	// Update is called once per frame
@@ -21,9 +22,12 @@ public class HeadTracking : MonoBehaviour
         transform.localPosition = headsetPos;
         transform.localRotation = headsetRot;
 
-        playerCollider.transform.localPosition = transform.localPosition;
+        //playerCollider.transform.localPosition = transform.localPosition;
         playerCollider.transform.rotation = Quaternion.identity;
-        playerCollider.height = headsetPos.y / transform.localScale.y;
-        playerCollider.center = new Vector3(playerCollider.center.x, headsetPos.y / 2 - playerCollider.height / 2, playerCollider.center.z);
+        playerCollider.height = headsetPos.y / transform.localScale.y; // Height is definitely right, center is wrong
+        // playerCollider height already adjusted for scale
+        playerCollider.center = new Vector3(playerCollider.center.x, - playerCollider.height / 2f + headCollider.radius, playerCollider.center.z);
+
+        //transform.parent.position = Vector3.zero;
     }
 }
